8736db8018ac14ad7bdfce3a1a6dab2bb8cd26b0
Christian <111076892+i-christian@users.noreply.github.com>
2023-09-28 23:37:44 +0200

    update bubble function

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,17 +9,21 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t outer_index, temp, inner_index;
+	size_t outer_index, inner_index;
+	int temp;
 
 	for (outer_index = 0; outer_index < size - 1; outer_index++)
 	{
-		for (inner_index = 0; size - outer_index - 1; inner_index++)
+		for (inner_index = 0; inner_index < size - outer_index - 1; inner_index++)
 		{
 			if (array[inner_index] > array[inner_index + 1])
 			{
 				temp = array[inner_index];
 				array[inner_index] = array[inner_index + 1];
 				array[inner_index + 1] = temp;
+
+				/* Print the array after each swap */
+				print_array(array, size);
 			}
 		}
 	}
