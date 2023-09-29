603050fadd7a4feaed9c915cfdf70f3abee532fc
Christian <111076892+i-christian@users.noreply.github.com>
2023-09-29 15:59:30 +0200

    insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list
+ * @list: param
+ * Return - Nothing
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *temp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	{
+		return;
+	}
+
+	for (current = (*list)->next; current != NULL; current = current->next)
+	{
+		temp = current;
+		while (temp->prev != NULL && temp->n < temp->prev->n)
+		{
+			temp->prev->next = temp->next;
+			if (temp->next != NULL)
+			{
+				temp->next->prev = temp->prev;
+			}
+			temp->next = temp->prev;
+			temp->prev = temp->prev->prev;
+			temp->next->prev = temp;
+
+			if (temp->prev != NULL)
+			{
+				temp->prev->next = temp;
+			}
+			else
+			{
+				*list = temp;
+			}
+
+			print_list(*list);
+		}
+	}
+}
+
