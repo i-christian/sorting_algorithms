4b7b4b0b778770aa0b081c475a1161a3d78935b8
Christian <111076892+i-christian@users.noreply.github.com>
2023-09-29 10:35:26 +0200

    optimise the bubble algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,6 +14,9 @@ void bubble_sort(int *array, size_t size)
 
 	for (outer_index = 0; outer_index < size - 1; outer_index++)
 	{
+		/*check if swapping occurs */
+		int swapped = 0;
+
 		for (inner_index = 0; inner_index < size - outer_index - 1; inner_index++)
 		{
 			if (array[inner_index] > array[inner_index + 1])
@@ -25,7 +28,13 @@ void bubble_sort(int *array, size_t size)
 
 				/* Print the array after each swap */
 				print_array(array, size);
+				swapped = 1;
 			}
+		}
+
+		if (swapped == 0)
+		{
+			break;
 		}
 	}
 }
