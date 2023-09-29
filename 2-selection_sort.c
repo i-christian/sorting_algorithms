47bab7321d9ce74cf064dbb24eb3b5ccf75b7b0e
Christian <111076892+i-christian@users.noreply.github.com>
2023-09-29 19:38:30 +0200

    selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,44 @@
+#include "sort.h"
+
+/**
+ * swap - interchanges values in an array
+ * @num1: parameter one
+ * @num2: paramenter two
+ * Return - void
+ */
+
+void swap(int *num1, int *num2)
+{
+	int temp = *num1;
+	*num1 = *num2;
+	*num2 = temp;
+}
+
+/**
+ * selection_sort - sorts an array
+ * @array: array to be sorted
+ * @size: the length of the array
+ * Return - Nothing (void)
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_value;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_value = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_value])
+			{
+				min_value = j;
+			}
+		}
+		if (min_value != i)
+		{
+			swap(&array[i], &array[min_value]);
+			print_array(array, size);
+		}
+	}
+}
